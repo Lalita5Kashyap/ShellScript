13c6e471b663eb8aae72c86eaf6fb045c0a2b1ac
Lalita Kashyap <lalitakashyaplk5@gmail.com>
2024-05-27 15:13:18 +0530

    Script 2

---
+++ WAS_Input_From_ArgumentsVariables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Write a Shell Script to take user input, input from arguments and print the variables.
+echo "What is your name"
+
+echo $1
--- WAS_Printa statement.sh
+++ WAS_Printa statement.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+#Write a Shell Script which prints I will complete #90DaysOofDevOps challenge
+
 echo "I will complete #90DaysOofDevOps challenge"
